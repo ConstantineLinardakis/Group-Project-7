86ac7bb42b694f3bcf2e47b9bcdc9d4db0a299d0
Euan9611 <73660684+Euan9611@users.noreply.github.com>
2020-12-09 11:20:03 -0700

    Create SoundButtons.java

---
+++ src/SoundButtons.java
@@ -0,0 +1,17 @@
+public class SoundButtons{
+  private void buttonSetSize(){
+  
+  }
+  private void buttonSetLocation(){
+  
+  }
+  private void setColor(){
+  
+  }
+  private void setSound(){
+  
+  }
+  private void getSound(){
+  
+  }
+}
