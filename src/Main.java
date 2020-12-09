60edadf9a72cee402052be42d83eee6865125c05
ethanbowles03 <48031234+ethanbowles03@users.noreply.github.com>
2020-12-09 11:41:21 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 public class Main{
+  //Opens the launchpad application
   private void openLaunchPad(){
   
   }
+  //Exits out of application
   private void exitApp(){
   
   }
+ /**
+ * Runs all the code for the program
+ * @param args command-line arguments
+ */
   public static void main(String[] args){
     
   }
