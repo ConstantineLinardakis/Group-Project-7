3962fcb6516aaf3c2d1dc673398ea86347c95492
ethanbowles03 <48031234+ethanbowles03@users.noreply.github.com>
2020-12-09 11:48:18 -0700

    Update SoundButtons.java

--- src/SoundButtons.java
+++ src/SoundButtons.java
@@ -1,16 +1,39 @@
 public class SoundButtons{
-  private void buttonSetSize(){
+  /**
+  *Sets the size of the buttons
+  *
+  *@param width - sets the width of the button
+  *@param height - sets the height of the button
+  */
+  private void buttonSetSize(int width, int height){
   
   }
-  private void buttonSetLocation(){
+  
+  /**
+  *Sets the location of the buttons
+  *
+  *@param locX - sets the x-location of the button
+  *@param locY - sets the y-location of the button
+  */
+  private void buttonSetLocation(int locX, int locY){
   
   }
-  private void setColor(){
+  
+  /**
+  *Sets the color of the buttons
+  *
+  *@param color - selects the color of the buttons
+  */
+  private void setColor(String[] color){
   
   }
+  
+  //Sets the sound on the launchpad
   private void setSound(){
   
   }
+  
+  //Gets the sounds on the laumchpad
   private void getSound(){
   
   }
