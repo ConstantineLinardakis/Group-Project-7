2d882c62d8fc217dbb48fb8eccf3feb73a84c11a
ethanbowles03 <48031234+ethanbowles03@users.noreply.github.com>
2020-12-09 11:43:19 -0700

    Update Buttons.java

--- src/Buttons.java
+++ src/Buttons.java
@@ -1,10 +1,14 @@
 public class Buttons{
+  //Displays the sound buttons on the launchpad
   private void displaySoundButtons(){
   
   }
+  //Displays the user interface buttons on the launch pad
   private void displayUIButtons(){
   
   }
+  //Displays the background and all other details of the launch pad such as background color
+  //Sets the dementions of the frame
   private void displayFrame(){
   
   }
