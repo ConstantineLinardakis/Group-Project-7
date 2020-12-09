399777da7edcfcfe96ef7ec6f0c7a987c528f3ce
Euan9611 <73660684+Euan9611@users.noreply.github.com>
2020-12-09 11:23:46 -0700

    Create UIButtons.java

---
+++ src/UIButtons.java
@@ -0,0 +1,14 @@
+public class UIButtons{
+  private void buttonSetSize(){
+  
+  }
+  private void buttonSetLocation(){
+  
+  }
+  private void usePressedButton(){
+  
+  }
+  private void setDetails(){
+  
+  }
+}
