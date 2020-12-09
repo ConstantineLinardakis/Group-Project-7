d5e789a6535cdc10d57bc080f120d527be68b429
ethanbowles03 <48031234+ethanbowles03@users.noreply.github.com>
2020-12-09 13:28:57 -0700

    Update ColorChangeButton.java

--- src/ColorChangeButton.java
+++ src/ColorChangeButton.java
@@ -1,7 +1,9 @@
 public class ColorChangeButton{
+  //Cycles through the colors on the launchpad allowing the user to change the color of it
   private void cycleThroughColors(){
   
   }
+  //Saves the arrangment of colors
   private void saveArrangment(){
   
   }
