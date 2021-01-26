67f5506e3f0c7bbc68713cf9fc7690b6e652b519
Constantine Linardakis <54334383+ConstantineLinardakis@users.noreply.github.com>
2021-01-26 11:49:45 -0700

    Update Sounds.java

--- src/Sounds.java
+++ src/Sounds.java
@@ -1,17 +1,24 @@
-public static synchronized void playSound(final String url) {
-  new Thread(new Runnable() {
-  // The wrapper thread is unnecessary, unless it blocks on the
-  // Clip finishing; see comments.
-    public void run() {
-      try {
-        Clip clip = AudioSystem.getClip();
-        AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+/* 
+   Pek -- Stack Overflow
+   Created playSound method.
+*/
+
+public class Sounds {
+  public static synchronized void playSound(final String url) {
+   new Thread(new Runnable() {
+   // The wrapper thread is unnecessary, unless it blocks on the
+    // Clip finishing; see comments.
+     public void run() {
+       try {
+          Clip clip = AudioSystem.getClip();
+          AudioInputStream inputStream = AudioSystem.getAudioInputStream(
           Main.class.getResourceAsStream("/path/to/sounds/" + url));
         clip.open(inputStream);
         clip.start(); 
-      } catch (Exception e) {
+        } catch (Exception e) {
         System.err.println(e.getMessage());
-      }
-    }
-  }).start();
+        }
+     }
+   }).start();
+  }
 }
