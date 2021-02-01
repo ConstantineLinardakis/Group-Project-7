8c0a375c63e151f87e97b15a550a41f1da1167ef
ethanbowles03 <48031234+ethanbowles03@users.noreply.github.com>
2021-02-01 11:05:59 -0700

    Update Launchpad.java

--- src/Launchpad.java
+++ src/Launchpad.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import javax.swing.JButton;
 
 //Main Class
 public class Launchpad{
@@ -6,6 +7,8 @@ public class Launchpad{
   private static Frame frame = new Frame("Launchpad");
   private static Buttons[][] btns = new Buttons[5][5];
   private static int colorCount = 0;
+  public static boolean loop = false;
+  
   public static void main(String[] args){
     //Orginal Button Location
     int locX = 20;
@@ -13,8 +16,9 @@ public class Launchpad{
     //Loops thorugh the buttons, creates them, defines dimentions and sets their original color
     for(int i = 0; i < 5; i++){
       for(int j = 0; j < 5; j++){
-        String title = i + ":" + j;
-        btns[i][j] = new Buttons(title);
+        int title = ((j + 1) + (i * 5));
+        String sTitle = Integer.toString(title);
+        btns[i][j] = new Buttons(sTitle);
         btns[i][j].setDimentions(locX + (j * 120),locY + (i * 120));
         btns[i][j].setColors("Gray", "Gray");
         frame.add(btns[i][j]);
@@ -23,6 +27,10 @@ public class Launchpad{
     //Adds the Change Colors button
     ColorButton cButton = new ColorButton();
     Labels labels = new Labels("C");
+    LoopButton loopButton = new LoopButton();
+    StopButton stopButton = new StopButton();
+    frame.add(stopButton);
+    frame.add(loopButton);
     frame.add(labels);
     frame.add(cButton);
 
@@ -37,7 +45,6 @@ public class Launchpad{
     for(int i = 0; i < 5; i++){
       for(int j = 0; j < 5; j++){
         frame.remove(btns[i][j]);
-        String title = i + ":" + j;
         btns[i][j].setDimentions(locX + (j * 120),locY + (i * 120));
         //Sets the colors of the buttons to the next array in the 2 dimentional array
         btns[i][j].setColors(color[colorCount][j + (i*5)], color[colorCount][j + (i*5)]);
@@ -46,11 +53,10 @@ public class Launchpad{
     }
     //Keeps track of which color array is being used
     colorCount++;
-    if(colorCount > 9){
+    if(colorCount > 10){
       colorCount = 0;
     }
     //Refreshes the frame after changes
     frame.repaint();
   }
 }
-
