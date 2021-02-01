87767f1e1cbcaa02e44712f8937de97f8e52a425
ethanbowles03 <48031234+ethanbowles03@users.noreply.github.com>
2021-02-01 11:06:53 -0700

    Add files via upload

---
+++ src/LoopButton.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.io.*;
+
+public class LoopButton extends JButton implements ActionListener{
+  public static boolean loop = false;
+  public LoopButton(){
+    super("Loop");
+    setBounds(120,10,100,50);
+    addActionListener(this);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e){
+    if(e.getSource() == this){
+      if(loop == true){
+        loop = false;
+      }else{
+        loop = true;
+      }
+    }
+  }
+}
